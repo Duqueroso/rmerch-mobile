c8c577733e0675138ebae353c5677aabcdcbd9ae
David Henao <daves.hb@hotmail.com>
2026-01-28 11:04:28 -0500

    commet resolved

--- app/(protected)/admin/layout.tsx
+++ app/(protected)/admin/layout.tsx
@@ -4,10 +4,6 @@ import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const AdminLayout = async ({ children }: any) => {
-  //   if (!user || user.role !== "admin") {
-  //     redirect("/dashboard");
-  //   }
-
   return (
     <SafeAreaView>
       <View>{children}</View>
